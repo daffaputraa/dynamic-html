b20be1451a5e80cd2844a9af0dc7373d26dd8821
daffaputraa <daffaputrapratamaa@gmail.com>
2024-11-07 15:01:16 +0700

    Fix : CSP add headers

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import DetailHalaman from "./pages/DetailHalaman";
 
-
 function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" exact element={<HomePage />} />
-          <Route path="/:id/:slug" exact element={<DetailHalaman />} />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/:id/:slug" element={<DetailHalaman />} />
+        <Route path="/:id" element={<DetailHalaman />} />
+      </Routes>
+    </Router>
   );
 }
 
